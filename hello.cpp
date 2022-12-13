dde75a64ec9d72f760dea4e9cceda7ed005cbcef
KaziSohail <112752223+KaziSohail@users.noreply.github.com>
2022-12-13 14:21:46 +0530

    introduction to cpp

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    // cout << "the value of x is : ";
+    cin >> x;
+    cout << x;
+    cout << "hello";
+    return 0;
+}
